a7539b41861c30b39d21de8983ac347f33cdb3cd
Haley Ristow <haleyhar44@yahoo.com>
2023-09-22 00:11:01 +0000

    initial repo commit

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
 import Link from "next/link";
-import Layout from"../components/layout";
+import Layout from "../components/layout";
 
-import { getSortedList } from "../lib/data";
+import { getSortedList } from "../lib/data-firebase";
 
-export async function getStaticProps(){
-  const allData = getSortedList();
+export async function getStaticProps() {
+  const allData = await getSortedList();
   return {
     props: { allData }
   };
 }
 
-export default function Home( {allData} ){
-  return(
+export default function Home({ allData }) {
+  return (
     <Layout home>
-    <h1>List of Contacts</h1>
+      <h1>List of Contacts</h1>
       <div className="list-group">
         {allData && allData.map(
-          ( {id, name} ) =>(
-            <Link key={id} href={`/${id}`} className="list-group-item list-group-item-action">
+          ({ id, name }) => (
+            <Link key={id} href={`/people/${id}`} className="list-group-item list-group-item-action">
               {name}
             </Link>
           )
         )
-          
+
         }
       </div>
-      </Layout>
+    </Layout>
   );
 }
